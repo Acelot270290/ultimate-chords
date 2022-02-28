31be3a6bbbabe5a3985eace60fbb60b3384042bf
Alan Diniz <adiniz@girafa.com.br>
2022-02-28 15:18:56 -0300

    Subindo a pagina coming soon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,90 +1,64 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
+<style>
+    body,
+    html {
+        height: 100%;
+        margin: 0;
+    }
 
-        <title>Laravel</title>
+    .bgimg {
+        background-image: url('https://cdn.pixabay.com/photo/2015/05/07/11/02/guitar-756326_960_720.jpg');
+        height: 100%;
+        background-position: center;
+        background-size: cover;
+        position: relative;
+        color: white;
+        font-family: "Courier New", Courier, monospace;
+        font-size: 25px;
+        opacity: 0.8
+    }
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    .topleft {
+        position: absolute;
+        top: 0;
+        left: 16px;
+    }
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    .bottomleft {
+        position: absolute;
+        bottom: 0;
+        left: 16px;
+    }
 
-            .full-height {
-                height: 100vh;
-            }
+    .middle {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        text-align: center;
+    }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    hr {
+        margin: auto;
+        width: 40%;
+    }
+</style>
 
-            .position-ref {
-                position: relative;
-            }
+<body>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+    <div class="bgimg">
+        <div class="topleft">
 
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Coming Soon
-                </div>
-
-            </div>
         </div>
-    </body>
+        <div class="middle">
+            <h1>COMING SOON</h1>
+            <hr>
+            <p>35 days left</p>
+        </div>
+
+    </div>
+
+</body>
+
 </html>
