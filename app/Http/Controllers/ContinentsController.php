d349ee2ad17451caafa66170d05213e88d8901ba
Alan Diniz <adiniz@girafa.com.br>
2022-03-03 19:16:46 -0300

    subindo atualização

---
+++ app/Http/Controllers/ContinentsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Continents;
+use Illuminate\Http\Request;
+
+class ContinentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Continents  $continents
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Continents $continents)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Continents  $continents
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Continents $continents)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Continents  $continents
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Continents $continents)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Continents  $continents
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Continents $continents)
+    {
+        //
+    }
+}
