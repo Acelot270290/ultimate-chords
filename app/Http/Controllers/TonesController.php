c95fa3e98d0bdc39e46d0473a418d199e6e75024
Artur Rossi <arturossi@github.com>
2022-03-04 18:20:53 -0300

    Criação de tabelas

---
+++ app/Http/Controllers/TonesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tones;
+use Illuminate\Http\Request;
+
+class TonesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Tones  $tones
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tones $tones)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Tones  $tones
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tones $tones)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Tones  $tones
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tones $tones)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Tones  $tones
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tones $tones)
+    {
+        //
+    }
+}
--- database/migrations/2022_01_04_000000_create_songs_table.php
+++ database/migrations/2022_01_04_000000_create_songs_table.php
@@ -15,33 +15,43 @@ class CreateSongsTable extends Migration
     {
         Schema::create('songs', function (Blueprint $table) {
             $table->bigIncrements('id_song');
-            $table->string('song_name', 250)->default('');
-            $table->string('song_tone', 3)->default('');
-            $table->boolean('chord')->default(0);
-            $table->boolean('tabs')->default(0);
-            $table->boolean('bass')->default(0);
-            $table->boolean('keyboard')->default(0);
-            $table->boolean('drums')->default(0);
 
             $table->unsignedBigInteger('song_id_artist', false)->nullable();
-            $table->unsignedBigInteger('song_id_videoclass', false)->nullable();
-            $table->unsignedBigInteger('song_id_style', false)->nullable();
-            
             // Chave Estrangeira Artista
             $table->foreign('song_id_artist', 'FK_id_artist_songs')
                 ->references('id_artist')
                 ->on('artists');
-
-            // Chave Estrangeira VideoAula
-            $table->foreign('song_id_videoclass', 'FK_id_videoclass_songs')
-                ->references('id_videoclass')
-                ->on('videoclasses');
             
+            $table->unsignedBigInteger('song_user_insertion_id', false)->nullable();
+            // Chave genero
+            $table->foreign('song_user_insertion_id', 'FK_id_user_songs')
+                ->references('id')
+                ->on('users');
+
+            $table->string('song_name', 250)->default('');
+            $table->year('song_year', 4)->default('0000');
+            $table->string('song_author', 250)->default('');
+
+            $table->unsignedBigInteger('song_id_style', false)->nullable();
             // Chave genero
             $table->foreign('song_id_style', 'FK_id_style_songs')
                 ->references('id_style')
                 ->on('styles');
+            
+            $table->string('song_tone', 3)->default('');
 
+            $table->unsignedBigInteger('song_id_videoclass', false)->nullable();
+            // Chave Estrangeira VideoAula
+            $table->foreign('song_id_videoclass', 'FK_id_videoclass_songs')
+                ->references('id_videoclass')
+                ->on('videoclasses');
+
+            $table->boolean('chord')->default(0);
+            $table->boolean('keyboard')->default(0);
+            $table->boolean('tabs')->default(0);
+            $table->boolean('bass')->default(0);
+            $table->boolean('drums')->default(0);
+            
             $table->timestamps();
         });
     }
