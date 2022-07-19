49dcbf854319ca4ee4d5be63be204837bba51c97
Artur Rossi <arturossi@github.com>
2022-07-19 20:09:49 -0300

    Adicionado início de página de música

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -105,6 +105,7 @@ class SongController extends Controller
             ->where('song_slug', '=', $song_slug)
             ->first();
         
+        $dataType = '';
         // Define qual é o tipo de cifra que vai ser utilizada.
         // O padrão é chords -> tabature -> bass -> keyboard -> drums
         if($song['chord']){
