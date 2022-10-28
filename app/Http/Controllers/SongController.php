9c913d30fd6b98190773c992c5cb22658751b38d
Alan <you@example.com>
2022-10-28 09:49:01 -0300

    Subindo correção

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -150,7 +150,7 @@ class SongController extends Controller
         ######################################################################################################
 
         // Le o arquivo json com os dados da música
-        $json = file_get_contents(public_path() . '/assets/db-all/' . $artist['id_artist'] . '/' . $song['id_song'] . '.json');
+        $json = file_get_contents(url('/assets/db-all/' . $artist['id_artist'] . '/' . $song['id_song'] . '.json'));
 
         // Muda o arquivo json para um array
         $json = json_decode($json, true);
